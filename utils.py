821771c222782216b1ed088829dabbeaeaec7cf2
Justin Sheldon <JSheldon3488@gmail.com>
2020-05-26 12:25:43 -0500

    Added OUNoise Class

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 """ This file contains all the utility classes """
 import random
-import torch
+import numpy as np
+import copy
 from collections import deque, namedtuple
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 class ReplayBuffer:
     """ Fixed Sized Replay buffer for storing and sampling experience tuples """
 
@@ -18,7 +19,7 @@ class ReplayBuffer:
         self.action_size = action_size
         self.buffer_size = buffer_size
         self.batch_size = batch_size
-        self.random_seed = random.seed(seed)
+        self.seed = random.seed(seed)
         self.memory = deque(max_len = self.buffer_size)
         self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
 
@@ -33,3 +34,26 @@ class ReplayBuffer:
     def __len__(self):
         " Return the current size of internal memory "
         return len(self.memory)
+
+
+class OUNoise:
+    """ Ornstein-Uhlenbeck Process """
+    def __init__(self, size, seed, mu=0., theta=0.15, sigma=0.2):
+        """ Initialize parameters and noise process """
+        self.mu = mu * np.ones(size)
+        self.theta = theta
+        self.sigma = sigma
+        self.seed = random.seed(seed)
+        self.reset()
+
+    def reset(self):
+        """ Reset the internal state noise and mean """
+        self.state = copy.copy(self.mu)
+
+    def sample(self):
+        """ Update internal state and return it as a noise sample """
+        x = self.state
+        dx = self.theta*(self.mu - x) + self.sigma*np.array([random.random() for i in range(len(x))])
+        self.state = x + dx
+        return self.state
+
