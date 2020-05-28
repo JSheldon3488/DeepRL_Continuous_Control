ada0d03c8ed5fd8e6c20d229bcbb90f5dca04e97
Justin Sheldon <JSheldon3488@gmail.com>
2020-05-28 15:53:57 -0500

    Updated Agent Utils

--- utils.py
+++ utils.py
@@ -7,27 +7,23 @@ import torch
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+
 class ReplayBuffer:
     """ Fixed Sized Replay buffer for storing and sampling experience tuples """
 
-    def __init__(self, action_size, buffer_size, batch_size, seed):
+    def __init__(self, buffer_size, batch_size):
         """ Initialize Replay Buffer
 
-        :param action_size:
-        :param buffer_size: max size of replay buffer
         :param batch_size: size of training sample
-        :param seed: random seed
         """
-        self.action_size = action_size
-        self.buffer_size = buffer_size
         self.batch_size = batch_size
-        self.seed = random.seed(seed)
+        self.buffer_size = buffer_size
         self.memory = deque(maxlen = self.buffer_size)
         self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
 
     def add(self, state, action, reward, next_state, done):
         """ Add an experience to memory """
-        self.memory.append(self.experience(state,action,reward,next_state,done))
+        self.memory.append(self.experience(state, action, reward, next_state, done))
 
     def sample(self):
         """ Sample batch_size of experiences randomly from memory """
@@ -48,12 +44,13 @@ class ReplayBuffer:
 
 class OUNoise:
     """ Ornstein-Uhlenbeck Process """
-    def __init__(self, size, seed, mu=0., theta=0.15, sigma=0.2):
+    def __init__(self, action_size, seed, mu=0., theta=0.15, sigma=0.2):
         """ Initialize parameters and noise process """
-        self.mu = mu * np.ones(size)
+        self.mu = mu * np.ones(action_size)
         self.theta = theta
         self.sigma = sigma
         self.seed = random.seed(seed)
+        self.state = None
         self.reset()
 
     def reset(self):
@@ -63,7 +60,7 @@ class OUNoise:
     def sample(self):
         """ Update internal state and return it as a noise sample """
         x = self.state
-        dx = self.theta*(self.mu - x) + self.sigma*np.array([random.random() for i in range(len(x))])
+        dx = self.theta*(self.mu - x) + self.sigma*np.array([np.random.randn() for i in range(len(x))])
         self.state = x + dx
         return self.state
 
